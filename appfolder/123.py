c231c9505147745b007f3ec2d38dec3d6385ecc7
anikasadiaOPT <anikasadia2723@gmail.com>
2026-07-23 00:04:31 +0600

    Updated app.py for folder path.

--- appfolder/123.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-YOLO_MODEL_PATH = BASE_DIR /"models" / "yolo" / "best.pt"
-
-print("Model path:", YOLO_MODEL_PATH)
-print("Exists:", YOLO_MODEL_PATH.exists())
